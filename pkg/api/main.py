20612d66d4ecce4d290b9994ce51c6bc0b4643d2
James Tufarelli <jspamless@gmail.com>
2023-11-06 21:40:00 +0000

    Add version to fast-api docs

--- pkg/api/main.py
+++ pkg/api/main.py
@@ -3,8 +3,16 @@ from fastapi import FastAPI, HTTPException
 import subprocess
 from fastapi.responses import PlainTextResponse
 import docker_router
+import os
 
-app = FastAPI()
+# Read version from environment variable or default to '0.0.0' if not set
+NAUTICAL_VERSION = os.getenv('NAUTICAL_VERSION', '0.0.0')
+
+app = FastAPI(
+    title="Nautical Backup",
+    summary="A simple Docker volume backup tool 🚀",
+    version=NAUTICAL_VERSION,
+)
 
 app.include_router(docker_router.router)
 
