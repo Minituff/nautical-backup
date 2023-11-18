2b8335123e260ebfc8a8a110f42069798b4e2065
James Tufarelli <jspamless@gmail.com>
2023-11-18 18:22:21 +0000

    Better dev commands

---
+++ .devcontainer/scripts/nb.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# Function to display help information
+show_help() {
+    cecho CYAN "Nautical Backup Developer Commands:"
+    echo "  build          - Build Nautical container"
+    echo "  run            - Run already built Nautical container"
+    echo "  build-run      - Build and run Nautical container"
+    echo ""
+    echo "  build-test     - Build and run Nautical Testing container"
+    echo "  test           - Run already built test Nautical container"
+    echo "  build-test-run - Build and run Nautical Testing container"
+    echo ""
+    echo "  api            - Run the Python API locally"
+    echo "  pytest         - Pytest locally and capture coverage"
+}
+
+APP_HOME="/workspaces/nautical-backup"
+export APP_HOME
+
+# Function to execute commands
+execute_command() {
+    case $1 in
+    build)
+        cecho CYAN "Building Nautical..."
+        cd $APP_HOME
+        docker build -t nautical-backup -t nautical-backup:test --no-cache --build-arg='NAUTICAL_VERSION=testing' .
+        ;;
+    build-run)
+        cd $APP_HOME
+        nb build
+        cecho CYAN "Running Nautical..."
+        cd dev
+        docker-compose up
+        ;;
+    run)
+        cecho CYAN "Running Nautical..."
+        cd $APP_HOME/dev
+        docker-compose up
+        ;;
+    build-test)
+        cecho CYAN "Building Test Nautical container..."
+        cd $APP_HOME
+        docker build -t minituff/nautical-test --no-cache --build-arg='NAUTICAL_VERSION=testing' --build-arg='TEST_MODE=0' .
+        ;;
+    test)
+        cecho CYAN "Running Test Nautical container..."
+        cd $APP_HOME/tests
+        docker compose run nautical-backup-test3
+        ;;
+    build-test-run)
+        nb build-test && cd $APP_HOME/tests
+        cecho CYAN "Running Test Nautical container..."
+        docker compose run nautical-backup-test3
+        ;;
+    api)
+        cd $APP_HOME
+        cecho CYAN "Running Test Nautical container..."
+        python3 -m uvicorn api.main:app --host 0.0.0.0 --port 8069 --use-colors
+        ;;
+    pytest)
+        cd $APP_HOME
+        clear
+        cecho CYAN "Running Pytest..."
+        python3 -m pytest --cov api --cov-report html --cov-report term
+        ;;
+    *)
+        echo "Unknown command: $1"
+        echo "Use 'nb --help' for a list of available commands."
+        ;;
+    esac
+}
+
+# Check for --help argument
+if [ -z "$1" ] || [ "$1" == "--help" ]; then
+    show_help
+else
+    execute_command "$1"
+fi
--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,55 +1,35 @@
 #!/usr/bin/env bash
 
-cecho() {
-  RED="\033[0;31m"
-  GREEN="\033[0;32m"  # <-- [0 means not bold
-  YELLOW="\033[1;33m" # <-- [1 means bold
-  CYAN="\033[1;36m"
-  # ... Add more colors if you like
+echo "Installing 'cecho' command..."
+ln -s /workspaces/nautical-backup/.devcontainer/scripts/cecho.sh /usr/bin/cecho
+chmod +x /usr/bin/cecho
 
-  NC="\033[0m" # No Color
-
-  # printf "${(P)1}${2} ${NC}\n" # <-- zsh
-  printf "${!1}${2} ${NC}\n" # <-- bash
-}
+cecho CYAN "Installing 'nb' command..."
+ln -s /workspaces/nautical-backup/.devcontainer/scripts/nb.sh /usr/bin/nb
+chmod +x /usr/bin/nb
 
 
-echo "Adding aliases (for convenience)..."
-
-# Go back to the workspace directory
-echo "alias home=\"cd /workspaces/nautical-backup\"" >> ~/.zshrc
-
-# BUILD container
-echo "alias nbb=\"home && docker build -t nautical-backup -t nautical-backup:test --no-cache --build-arg='NAUTICAL_VERSION=testing' .\"" >> ~/.zshrc
-
-# BUILD & RUN container
-echo "alias nbbr=\"nbb && cd dev && docker-compose up\"" >> ~/.zshrc
-
-# RUN container that is already build
-echo "alias nbr=\"home/dev && docker-compose up\"" >> ~/.zshrc
-
-# BUILD TEST container
-echo "alias nbt=\"home && docker build -t minituff/nautical-test --no-cache --build-arg='NAUTICAL_VERSION=testing' --build-arg='TEST_MODE=0' .\"" >> ~/.zshrc
-
-# BUILD & RUN TEST container
-echo "alias nbtr=\"nbt && cd /workspaces/nautical-backup/tests && docker compose run nautical-backup-test3\"" >> ~/.zshrc
-
-# RUN API locally
-echo "alias nbapi=\"python3 -m uvicorn api.main:app --host 0.0.0.0 --port 8069 --use-colors\"" >> ~/.zshrc
-
-# Run pytest and output report as hmtl
-echo "alias nbpt=\"home && && clear && python3 -m pytest --cov api --cov-report html --cov-report term\"" >> ~/.zshrc
-
-
-cecho "CYAN" "Installing python packages (for api)..."
+cecho CYAN "Installing python packages (for api)..."
 python3 -m pip install -r /workspaces/nautical-backup/api/requirements.txt
-cecho "CYAN" "Installing python packages (for api tests)..."
-python3 -m pip install -r /workspaces/nautical-backup/pytest/requirements.txt
-cecho "CYAN" "Installing python packages (for docs)..."
-python3 -m pip install -r /workspaces/nautical-backup/docs/requirements.txt
-python3 -m pip install black
 
-cecho "GREEN" "-- Init complete -- Nautical development enviornment ready to go!!"
+cecho CYAN "Installing python packages (for api tests)..."
+python3 -m pip install -r /workspaces/nautical-backup/pytest/requirements.txt
+
+cecho CYAN "Installing python packages (for docs)..."
+python3 -m pip install -r /workspaces/nautical-backup/docs/requirements.txt
+
+cecho CYAN "Handling locales..."
+echo "export LANG=en_US.UTF-8" >> ~/.zshrc; 
+LC_CTYPE=en_US.UTF-8
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen
+locale-gen
+
+# Test commands
+nb --help
+
+cecho "GREEN" "Success!! Nautical Development enviornment ready to go!!"
+cecho "GREEN" "Use the command 'nb --help' to get started."
+
 
 zsh && omz reload
 # No need to 'source ~/.zshrc' since the terminal won't be open yet
