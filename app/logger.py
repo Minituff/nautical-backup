cb8353201bc9249b5e635c11121f6b0382980cef
James Tufarelli <jspamless@gmail.com>
2024-03-23 11:52:29 -0700

    Add logger

---
+++ app/logger.py
@@ -0,0 +1,54 @@
+import datetime
+import os
+
+
+class Logger:
+    def __init__(self):
+        self.levels = {"TRACE": 0, "DEBUG": 1, "INFO": 2, "WARN": 3, "ERROR": 4}
+
+        # Defaults
+        self.script_logging_level = "INFO"
+        self.report_file_logging_level = "INFO"
+        self.report_file_on_backup_only = "true"
+
+        # Override the defaults with environment variables if they exist
+        self.script_logging_level = os.environ.get("LOG_LEVEL", self.script_logging_level)
+        self.report_file_logging_level = os.environ.get("REPORT_FILE_LOG_LEVEL", self.report_file_logging_level)
+        self.report_file_on_backup_only = os.environ.get("REPORT_FILE_ON_BACKUP_ONLY", self.report_file_on_backup_only)
+
+        self.dest_location = os.environ.get("DEST_LOCATION", "")
+        self.report_file = f"Backup Report - {datetime.datetime.now().strftime('%Y-%m-%d')}.txt"
+
+    def delete_report_file(self):
+        for file in os.listdir(self.dest_location):
+            if file.startswith("Backup Report -") and file.endswith(".txt"):
+                os.remove(os.path.join(self.dest_location, file))
+
+    def create_new_report_file(self):
+        if self.report_file_on_backup_only == "true":
+            self.delete_report_file()
+            # Initialize the current report file with a header
+            with open(os.path.join(self.dest_location, self.report_file), 'w') as f:
+                f.write(f"Backup Report - {datetime.datetime.now()}\n")
+
+    def log_this(self, log_message, log_priority="INFO", message_type="default"):
+        # Check if level exists
+        if log_priority not in self.levels:
+            return
+
+        # Check if level is enough for console logging
+        if self.levels[log_priority] >= self.levels[self.script_logging_level]:
+            print(f"{log_priority}: {log_message}")
+
+        # Check if level is enough for report file logging
+        if self.report_file_on_backup_only == "true" and self.levels[log_priority] >= self.levels[self.report_file_logging_level]:
+            if not (message_type == "init" and self.report_file_on_backup_only == "true"):
+                with open(os.path.join(self.dest_location, self.report_file), 'a') as f:
+                    f.write(f"{datetime.datetime.now()} - {log_priority}: {log_message}\n")
+
+if __name__ == "__main__":
+    # Example usage
+    logger = Logger()
+    logger.create_new_report_file()
+    logger.log_this("This is an info message")
+    logger.log_this("This is a debug message", log_priority="DEBUG")
