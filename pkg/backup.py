5f9a532dce2f9d8bea21971711eac71799f8d847
James Tufarelli <jspamless@gmail.com>
2024-03-23 01:05:23 +0000

    Allow nautical to be run from bash commands

--- pkg/backup.py
+++ pkg/backup.py
@@ -1,3 +1,12 @@
+#!/usr/local/bin/python
+# Or whatever path to your python interpreter
+
+# TODO: Add the following lines to your Dockerfile
+# ln -s /workspaces/nautical-backup/pkg/backup.py /usr/local/bin/script-test
+# chmod +x /usr/local/bin/script-test
+
 from api.db import DB
 from api.config import Settings
 from typing import Union
+
+print("TESTING: pkg.backup.py")
