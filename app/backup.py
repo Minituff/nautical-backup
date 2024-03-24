d6f827146c4d3c227600a98a76ec2eab3b78ad61
James Tufarelli <jspamless@gmail.com>
2024-03-24 18:34:16 +0000

    Add build-out for rsync command

--- app/backup.py
+++ app/backup.py
@@ -7,9 +7,10 @@
 
 import os
 from datetime import datetime
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
 import sys
 import subprocess
+from pathlib import Path
 
 import docker
 from docker.models.containers import Container
@@ -24,12 +25,12 @@ from enum import Enum
 class BeforeOrAfter(Enum):
     BEFORE = 1
     AFTER = 2
-    
+
 class BeforeAfterorDuring(Enum):
     BEFORE = 1
     AFTER = 2
     DURING = 3
-    
+
 class NauticalBackup:
     def __init__(self):
         self.db = DB()
@@ -37,6 +38,8 @@ class NauticalBackup:
         self.logger = Logger()
         self.settings = Settings()
         self.docker = docker.from_env()
+        # The prefix and suffix used to define this group
+        self.default_group_pfx_sfx = "&"
 
         self.verify_source_location(self.env.SOURCE_LOCATION)
         self.verify_destination_location(self.env.DEST_LOCATION)
@@ -125,7 +128,7 @@ class NauticalBackup:
             if self._should_skip_container(c) == True:
                 continue  # Skip this container
             # Create a default group, so ungrouped items are not grouped together
-            default_group = f"_{str(c.id)[0:12]}"
+            default_group = f"{self.default_group_pfx_sfx}{str(c.id)[0:12]}{self.default_group_pfx_sfx}"
             group = str(c.labels.get("nautical-backup.group", default_group))
 
             if group not in containers_by_group:
@@ -160,7 +163,6 @@ class NauticalBackup:
         self.log_this(f"Running CURL command: {command}")
         out = subprocess.run(command, shell=True, executable="/bin/bash", capture_output=False)
         return out
-
 
     def _run_lifecyle_hook(self, c: Container, when: BeforeOrAfter):
         """Runs a commend inside the child container"""
@@ -216,36 +218,171 @@ class NauticalBackup:
                 self.log_this(f"Container {c.name} was not in exited state. Trying again (Attempt {attempt}/3)", "ERROR")
                 self._stop_container(c, attempt=attempt+1)
             return False
+        
+    def _start_container(self, c:Container, attempt=1) -> bool:
+            c.reload() # Refresh the status for this container
             
+            if c.status != "exited":
+                self.log_this(f"Container {c.name} was not stopped. No need to start.", "DEBUG")
+                return True
+            
+            try:
+                self.log_this(f"Starting {c.name}...")
+                c.start() #* Actually stop the container
+            except APIError as e:
+                self.log_this(f"Error starting container {c.name}.", "ERROR")
+                return False
+            
+            c.reload() # Refresh the status for this container
+            
+            if c.status == "running":
+                return True
+            elif attempt <= 3:
+                self.log_this(f"Container {c.name} was not in running state. Trying again (Attempt {attempt}/3)", "ERROR")
+                self._start_container(c, attempt=attempt+1)
+            return False
+    
+    def _get_src_dir(self, c: Container) -> Tuple[Path, str]:
+        base_src_dir = Path(self.env.SOURCE_LOCATION)
+        src_dir_no_path = str(c.name)
+        src_dir: Path = base_src_dir / src_dir_no_path
+        
+        if str(c.name) in self.env.OVERRIDE_SOURCE_DIR:
+            new_src_dir = self.env.OVERRIDE_SOURCE_DIR[str(c.name)]
+            src_dir = base_src_dir / new_src_dir
+            src_dir_no_path = new_src_dir
+            self.log_this(f"Overriding source directory for {c.name} to '{new_src_dir}'")
+        
+        if str(c.id) in self.env.OVERRIDE_SOURCE_DIR:
+            new_src_dir = self.env.OVERRIDE_SOURCE_DIR[str(c.id)]
+            src_dir = base_src_dir / new_src_dir
+            src_dir_no_path = new_src_dir
+            self.log_this(f"Overriding source directory for {c.id} to '{new_src_dir}'")
+        
+        label_src = str(c.labels.get("nautical-backup.override-source-dir", ""))
+        if label_src and label_src != "":
+            self.log_this(f"Overriding source directory for {c.name} to '{label_src}' from label")
+            src_dir = base_src_dir / label_src
+            src_dir_no_path = label_src
+        
+        return src_dir, src_dir_no_path
+    
+    def _get_dest_dir(self, c: Container, src_dir_name: str) -> Path:
+        base_dest_dir = Path(self.env.DEST_LOCATION)
+        dest_dir: Path = base_dest_dir / str(c.name)
+        
+        keep_src_dir_name_label = str(c.labels.get("nautical-backup.keep_src_dir_name", "true")).lower()
+        if keep_src_dir_name_label == "true":
+            dest_dir: Path = base_dest_dir / src_dir_name
+        
+        if str(c.name) in self.env.OVERRIDE_DEST_DIR:
+            new_dest_dir = self.env.OVERRIDE_DEST_DIR[str(c.name)]
+            dest_dir = base_dest_dir / new_dest_dir
+            self.log_this(f"Overriding destination directory for {c.name} to '{new_dest_dir}'")
+        
+        if str(c.id) in self.env.OVERRIDE_DEST_DIR:
+            new_dest_dir = self.env.OVERRIDE_DEST_DIR[str(c.id)]
+            dest_dir = base_dest_dir / new_dest_dir
+            self.log_this(f"Overriding destination directory for {c.id} to '{new_dest_dir}'")
+        
+        label_dest = str(c.labels.get("nautical-backup.override-destination-dir", ""))
+        if label_dest and label_dest != "":
+            self.log_this(f"Overriding destination directory for {c.name} to '{label_dest}' from label")
+            dest_dir = base_dest_dir / label_dest
+        
+        return dest_dir
+    
+    def _backup_additional_folders(self, c: Container):
+        pass
+
+    def _backup_main_folder(self, c: Container):
+        src_dir, src_dir_no_path = self._get_src_dir(c)
+        
+        dest_dir = self._get_dest_dir(c, src_dir_no_path)
+        
+        self.log_this(f"Backing up {c.name} data..." "INFO")
+        
+        rsync_args = self._get_rsync_args(c)
+        self._ryn_rsync(c, rsync_args, src_dir, dest_dir)
+    
+    def _ryn_rsync(self, c: Container, rsync_args: str, src_dir: Path, dest_dir: Path):
+        src_folder = f"{src_dir.absolute()}/"
+        dest_folder = f"{dest_dir.absolute()}/"
+        
+        command = f"{rsync_args} {src_folder} {dest_folder}"
+        
+        self.log_this(f"RUNNING: 'rsync {command}'", "INFO")
+        
+        out = subprocess.run(command, shell=True, executable="/usr/bin/rsync", capture_output=False)
+        
+    def _get_rsync_args(self, c: Container) -> str:
+        default_rsync_args = self.env.DEFAULT_RNC_ARGS
+        
+        if str(self.env.USE_DEFAULT_RSYNC_ARGS).lower() == "false":
+            self.log_this(f"Disabling default rsync arguments ({self.env.DEFAULT_RNC_ARGS})", "DEBUG")
+            default_rsync_args = ""
+        
+        use_default_args = str(c.labels.get("nautical-backup.use-default-rsync-args", "")).lower()
+        if use_default_args == "false":
+            self.log_this(f"Disabling default rsync arguments ({self.env.DEFAULT_RNC_ARGS})", "DEBUG")
+            default_rsync_args = ""
+        
+        if str(self.env.RSYNC_CUSTOM_ARGS) != "":
+            custom_rsync_args = str(self.env.RSYNC_CUSTOM_ARGS)
+            self.log_this(f"Adding custom rsync arguments ({custom_rsync_args})", "DEBUG")
+        
+        custom_rsync_args = str(c.labels.get("nautical-backup.rsync-custom-args", "")).lower()
+        if custom_rsync_args != "":
+            self.log_this(f"Disabling default rsync arguments ({self.env.DEFAULT_RNC_ARGS})", "DEBUG")
+            custom_rsync_args = ""
+        
+        return f"{default_rsync_args} {custom_rsync_args}"
+
+
     def backup(self):
         self.db.put("backup_running", True)
         self.db.put("last_cron", datetime.now().strftime("%m/%d/%y %I:%M"))
 
         containers_by_group = self.group_containers()
 
-        # Before backup
         for group, containers in containers_by_group.items():
+            # No need to print group for individual containers
+            if not group.startswith(self.default_group_pfx_sfx) and not group.endswith(self.default_group_pfx_sfx):
+                self.log_this(f"Backing up group: {group}")
+                
+            # Before backup
             for c in containers:
                 # Run before hooks
                 self._run_curl(c, BeforeAfterorDuring.BEFORE)
                 self._run_lifecyle_hook(c, BeforeOrAfter.BEFORE)
-
-                stop_result = self._stop_container(c)
-                # Stop containers
-                pass
+                
+                src_dir, src_dir_no_path = self._get_src_dir(c)
+                src_dir_required = str(c.labels.get("nautical-backup.source-dir-required-to-stop", "true")).lower()
+                if not src_dir.exists():
+                    if src_dir_required == "false":
+                        self.log_this(f"{c.name} - Source directory $src_dir does, but that's okay", "DEBUG")
+                else:
+                    self.log_this(f"{c.name} - Source directory $src_dir does not exist. Skipping", "DEBUG")
+                    continue
+                
+                stop_result = self._stop_container(c)  # Stop containers
             
-        # During backup
-        for group, containers in containers_by_group.items():
+            # During backup
             for c in containers:
-                c.reload() # Refresh the status for this container
                 # Backup containers
-                # if c.status == "exited" or label says its okay
-                    # self._backup_now()
-                pass
-        # After backup
-        for group, containers in containers_by_group.items():
+                c.reload() # Refresh the status for this container
+                if c.status != "exited":
+                    stop_before_backup = str(c.labels.get("nautical-backup.stop-before-backup", "true"))
+                    if stop_before_backup.lower() == "true":
+                        self.log_this(f"Skipping backup {c.name} because it was not stopped" "WARN")
+                        continue
+            
+            self._backup_main_folder(c)
+            
+            # After backup
             for c in containers:
-                # Start containers
+                
+                start_result = self._start_container(c)  # Start containers
                 
                 self._run_lifecyle_hook(c, BeforeOrAfter.AFTER)
                 self._run_curl(c, BeforeAfterorDuring.AFTER)
--- app/nautical_env.py
+++ app/nautical_env.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 
 class NauticalEnv:
     def __init__(self) -> None:
@@ -13,7 +14,46 @@ class NauticalEnv:
         self.DEST_LOCATION = os.environ.get("DEST_LOCATION", "")
         self.SOURCE_LOCATION = os.environ.get("SOURCE_LOCATION", "")
         
+        self.KEEP_SRC_DIR_NAME = os.environ.get("KEEP_SRC_DIR_NAME", "")
+        
+        self.OVERRIDE_SOURCE_DIR =  self._populate_override_dirs("OVERRIDE_SOURCE_DIR")
+        self.OVERRIDE_DEST_DIR =  self._populate_override_dirs("OVERRIDE_DEST_DIR")
+        
+        self.DEFAULT_RNC_ARGS = "-ahq" # Default
+        self.USE_DEFAULT_RSYNC_ARGS = os.environ.get("USE_DEFAULT_RSYNC_ARGS", "")
+        self.RSYNC_CUSTOM_ARGS = os.environ.get("RSYNC_CUSTOM_ARGS", "")
         
         self.REQUIRE_LABEL = False
         if os.environ.get("REQUIRE_LABEL", "False").lower() == True:
             self.REQUIRE_LABEL = True 
+    
+    @ staticmethod
+    def _populate_override_dirs(env_name: str) ->  Dict[str, str]:
+        """Translate the Enviornment variable from single string to Python Dict.
+        
+        ```
+        input="example1:example1-new-source-data,ctr2:ctr2-new-source"
+        
+        output = {
+            "example1": "example1-new-source-data",
+            "ctr2": "ctr2-new-source"
+        }
+        ```
+        """
+        raw = str(os.environ.get(env_name, ""))
+        
+        result = {}
+        
+        if not raw:
+            return result
+        
+        for pair in raw.split(","):
+            split = pair.split(":")
+            if len(split) < 2:
+                continue
+            
+            container_name = str(split[0])
+            new_dir = str(split[1])
+            result[container_name] = new_dir
+        
+        return result
