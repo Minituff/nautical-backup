a6ba014d53edb9d68274e2dd2cfb189d7d2ba874
James Tufarelli <jspamless@gmail.com>
2024-03-25 01:53:02 +0000

    Better extraction of docker tests

--- pytest/test_backup.py
+++ pytest/test_backup.py
@@ -11,11 +11,22 @@ from app.nautical_env import NauticalEnv
 from app.backup import NauticalBackup
 
 
-class TestBackup:
-    global default_labels
+@pytest.fixture
+def docker_mocks(request: pytest.FixtureRequest) -> tuple[MagicMock, MagicMock, MagicMock]:
+    """Creates a mock Docker client and two mock containers.
+
+    To use:
+    ```
+    def test_docker_run(self, docker_mocks):
+        mockDockerClient, mockContainer1, mockContainer2 = docker_mocks
+        mockDockerClient.containers.list.return_value = [mockContainer1, mockContainer2]
+        # Do something...
+    ```
+    """
+    # Set defaults
     default_labels = {
         "nautical-backup.group": "",
-        "nautical-backup.additional-folders.when": "during",
+        "nautical-backup.additional-folders.when": "",
         "nautical-backup.additional-folders": "",
         "nautical-backup.rsync-custom-args": "",
         "nautical-backup.use-default-rsync-args": "",
@@ -23,43 +34,59 @@ class TestBackup:
         "nautical-backup.override-destination-dir": "",
     }
 
+    labels = dict(request.param.get("labels", default_labels))
+
+    # These status are exactly what is required for the backup to run
+    status_side_effect = request.param.get("status_side_effect", ["running", "exited", "exited", "exited", "running"])
+
+    # Create mocks
+
+    mockDockerClient = MagicMock(spec=docker.DockerClient)
+    mockContainer1 = MagicMock(spec=Container)
+    mockContainer1.name = "container1"
+    mockContainer1.id = "1234456789"
+    type(mockContainer1).status = PropertyMock(side_effect=cycle(status_side_effect))
+
+    def fake_labels(key, default=None):
+        return labels.get(key, default)
+
+    mockContainer1.labels.get.side_effect = fake_labels
+
+    mockContainer2 = MagicMock(spec=Container)
+    mockContainer2.name = "container2"
+    mockContainer2.id = "9876543210"
+    type(mockContainer1).status = PropertyMock(side_effect=cycle(status_side_effect))
+    mockContainer2.labels.get.side_effect = fake_labels
+
+    return mockDockerClient, mockContainer1, mockContainer2
+
+
+class TestBackup:
+    # Default parameters for the entire class
+    pytestmark = pytest.mark.parametrize(
+        "docker_mocks",
+        [
+            {
+                # "status_side_effect": ["running", "exited", "exited", "exited", "running"],
+            },
+        ],
+        indirect=["docker_mocks"],
+    )
+
     @classmethod
     def setup_class(cls):
         """Runs 1 time before all tests in this class"""
         pass
 
-    def test_docker_run(
-        self,
-    ):
-        # Create a mock container instance
-        mockDockerClient = MagicMock(spec=docker.DockerClient)
-
-        # Set up the mock labels dictionary and its get method
-        def fake_labels(*args):
-            if args[0] in default_labels:
-                return default_labels[args[0]]
-
-            if args[1]:  # Default value
-                return args[1]
-
-        mockContainer1 = MagicMock(spec=Container)
-        mockContainer1.name = "container1"
-        mockContainer1.id = "1234456789"
-
-        # These statuses are in the exact order as expected throughout the lifecycle of the backup
-        type(mockContainer1).status = PropertyMock(
-            side_effect=cycle(["running", "exited", "exited", "exited", "running"])
-        )
-
-        mockContainer1.labels.get.side_effect = fake_labels
-
-        mockDockerClient.containers.list.return_value = [mockContainer1]
+    def test_docker_run(self, docker_mocks):
+        mockDockerClient, mockContainer1, mockContainer2 = docker_mocks
+        mockDockerClient.containers.list.return_value = [mockContainer1, mockContainer2]
 
         nb = NauticalBackup(mockDockerClient)
         nb.backup()
 
-        print(mockDockerClient.call_count)
 
+class TestRsync:
     def test_rsync_commands(self, monkeypatch: pytest.MonkeyPatch):
 
         # Define the source location
