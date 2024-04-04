6074f33adc9b5b6cf147dd4f702e16887b186f11
James Tufarelli <jspamless@gmail.com>
2024-04-04 20:23:47 +0000

    Disable rsync tests

--- pytest/test_rsync.py
+++ pytest/test_rsync.py
@@ -7,19 +7,19 @@ from app.nautical_env import NauticalEnv
 
         
         
-class TestRsync:
-    def test_rsync_commands(self, monkeypatch: pytest.MonkeyPatch):
+# class TestRsync:
+#     def test_rsync_commands(self, monkeypatch: pytest.MonkeyPatch):
 
-        # Define the source location
-        monkeypatch.setenv("DEST_LOCATION", "./tests/destination")
-        monkeypatch.setenv("SOURCE_LOCATION", "./tests/source")
+#         # Define the source location
+#         monkeypatch.setenv("DEST_LOCATION", "./tests/destination")
+#         monkeypatch.setenv("SOURCE_LOCATION", "./tests/source")
 
-        env = NauticalEnv()
-        SOURCE_LOCATION = env.SOURCE_LOCATION
+#         env = NauticalEnv()
+#         SOURCE_LOCATION = env.SOURCE_LOCATION
 
-        # Create directories and files
-        Path(SOURCE_LOCATION, "container1").mkdir(parents=True, exist_ok=True)
-        Path(SOURCE_LOCATION, "container1", "test.txt").touch()
+#         # Create directories and files
+#         Path(SOURCE_LOCATION, "container1").mkdir(parents=True, exist_ok=True)
+#         Path(SOURCE_LOCATION, "container1", "test.txt").touch()
 
-        Path(SOURCE_LOCATION, "container2").mkdir(parents=True, exist_ok=True)
-        Path(SOURCE_LOCATION, "container1", "test.txt").touch()
+#         Path(SOURCE_LOCATION, "container2").mkdir(parents=True, exist_ok=True)
+#         Path(SOURCE_LOCATION, "container1", "test.txt").touch()
