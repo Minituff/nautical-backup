ecec814fa9913a24723fd78d3e9a3a12add5e108
James Tufarelli <jspamless@gmail.com>
2024-04-10 18:04:44 +0000

    Remove integration test3 This is the old bash tests that have been converted to python

--- s6-overlay/etc/s6-overlay/s6-rc.d/ci-test/run.sh
+++ s6-overlay/etc/s6-overlay/s6-rc.d/ci-test/run.sh
@@ -10,22 +10,22 @@ if [ "$TEST_MODE" != "-1" ]; then
 
     # Run the tests and capture their exit code
     if [ "$TEST_MODE" == "1" ]; then
-        with-contenv bash /tests/_env-tests.sh test1
+        with-contenv bash /tests/_integration_tests test1
         exit_code=$?
     elif [ "$TEST_MODE" == "2" ]; then
-        with-contenv bash /tests/_env-tests.sh test2
+        with-contenv bash /tests/_integration_tests test2
         exit_code=$?
     elif [ "$TEST_MODE" == "3" ]; then
         cd /tests       # The .simplecov must be detected in the directory from where the bashcov command is run from
         rm -rf /coverage/* # Remove the coverage (if it exists)
-        with-contenv bashcov /tests/_tests.sh
+        # with-contenv bashcov /tests/_tests.sh
         exit_code=$?
 
         bash /tests/_fix_coverage_paths.sh
     elif [ "$TEST_MODE" == "4" ]; then
         cd /tests       # The .simplecov must be detected in the directory from where the bashcov command is run from
         rm -rf /coverage/* # Remove the coverage (if it exists)
-        with-contenv bashcov /tests/_tests.sh
+        # with-contenv bashcov /tests/_tests.sh
         exit_code=$?
     else
         echo "UNKNOWN TEST MODE: ${TEST_MODE}"
