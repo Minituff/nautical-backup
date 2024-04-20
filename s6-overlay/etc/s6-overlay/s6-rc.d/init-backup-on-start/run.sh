14c1562e25eb7b962352b4410a59d90fa163a2f7
James Tufarelli <jspamless@gmail.com>
2024-04-20 18:42:58 +0000

    Update log

--- s6-overlay/etc/s6-overlay/s6-rc.d/init-backup-on-start/run.sh
+++ s6-overlay/etc/s6-overlay/s6-rc.d/init-backup-on-start/run.sh
@@ -4,7 +4,7 @@ source /app/logger.sh # Use the logger script
 
 if [ "$BACKUP_ON_START" = "true" ]; then
     logThis "Starting backup since BACKUP_ON_START is true" "INFO" "init"
-    logThis "Note - BACKUP_ON_START logs are not available until all containers are processed" "INFO" "init"
+    logThis "Note - BACKUP_ON_START logs are not available until all containers are processed, however the report file updates in real-time." "INFO" "init"
     # The backup script must be run from the root directory
     cd /
     # python3 /app/backup.py
